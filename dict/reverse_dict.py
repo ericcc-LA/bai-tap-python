aaefa0705d71229d34b0f9c365d3787f57358036
Quan <minhquanne0304@gmail.com>
2026-06-11 16:29:35 +0700

    fix: coding convention

--- dict/reverse_dict.py
+++ dict/reverse_dict.py
@@ -1,17 +1,11 @@
 
-
 def reverse_dict(d):
-
     result = {}
-
     for key, value in d.items():
-
         result[value] = key
-
     return result
 
 if __name__ == "__main__ ":
-
     print(reverse_dict({ "a": 1 ,"b" :2 ,"c" :3}))
 
 
